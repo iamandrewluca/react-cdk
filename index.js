24042bb1146c17b7315c69f55e3359887c4d0a22
Andrew Luca <thendrluca@gmail.com>
2021-10-29 11:17:26 +0300

    add both ways of init

--- index.js
+++ index.js
@@ -18,14 +18,17 @@ const isConstruct = type => Construct.isPrototypeOf(type) || type === Construct
  * for big element trees this can stack overflow ?!
  */
 function render(element, parentConstruct) {
+	if (Array.isArray(element) && parentConstruct === undefined) {
+		throw new Error('rendering an array of construct elements needs a parentConstruct')
+	}
+
 	if (Array.isArray(element)) {
 		element.forEach(child => render(child, parentConstruct))
 		return parentConstruct
 	}
 
 	if (element.type === Fragment) {
-		render(element.props.children, parentConstruct)
-		return parentConstruct
+		return render(element.props.children, parentConstruct)
 	}
 
 	if (isConstruct(element.type)) {
@@ -59,31 +62,36 @@ function FunctionExample({children}) {
 	)
 }
 
-/**
- * App from @aws-cdk/core does not exactly follow Construct constructor
- * https://github.com/aws/aws-cdk/blob/0d7452ee3ce22179242241ed85cf55a173af19b5/packages/%40aws-cdk/core/lib/app.ts#L108
- */
-class App extends Construct {
-}
+class App extends Construct {}
 
 const element = (
-	<App>
-		{/* Path: top-construct */}
-		<Construct key="top-construct">
-			<FunctionExample>
-				{/* Path: top-construct/construct-from-function-2/inner-construct-1 */}
-				<Construct key="inner-construct-1"/>
-				{/* Path: top-construct/construct-from-function-2/inner-construct-2 */}
-				<Construct key="inner-construct-2"/>
-			</FunctionExample>
-		</Construct>
-	</App>
+	//	Path: top-construct
+	<Construct key="top-construct">
+		<FunctionExample>
+			{/* Path: top-construct/construct-from-function-2/inner-construct-1 */}
+			<Construct key="inner-construct-1"/>
+			{/* Path: top-construct/construct-from-function-2/inner-construct-2 */}
+			<Construct key="inner-construct-2"/>
+		</FunctionExample>
+	</Construct>
 )
 
+{
+	/**
+	 * App from @aws-cdk/core does not exactly follow Construct constructor
+	 * https://github.com/aws/aws-cdk/blob/0d7452ee3ce22179242241ed85cf55a173af19b5/packages/%40aws-cdk/core/lib/app.ts#L108
+	 * So this will not be possible
+	 */
+	const app = render(<App>{element}</App>)
+	console.log(app);
+}
 
-const app = render(element)
-
-console.log(app);
+{
+	const app = new App(undefined, undefined)
+	const output = render(element, app)
+	console.log(app);
+	console.log(app === output);
+}
 
 
 
