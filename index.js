b6b1e2660f31c8b19a26bd3a3de1d8e9d5e802e2
Andrew Luca <thendrluca@gmail.com>
2021-10-29 11:13:46 +0300

    return construct from render

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const isConstruct = type => Construct.isPrototypeOf(type) || type === Construct
 function render(element, parentConstruct) {
 	if (Array.isArray(element)) {
 		element.forEach(child => render(child, parentConstruct))
-		return
+		return parentConstruct
 	}
 
 	if (element.type === Fragment) {
 		render(element.props.children, parentConstruct)
-		return
+		return parentConstruct
 	}
 
 	if (isConstruct(element.type)) {
@@ -33,15 +33,14 @@ function render(element, parentConstruct) {
 		const construct = new element.type(parentConstruct, key, rest)
 		console.log(Node.of(construct).path);
 		render(children, construct)
-		return
+		return parentConstruct ?? construct
 	}
 
 	// Keep this below isConstruct check
 	if (element.type instanceof Function) {
 		const {type, props} = element
 		const newElement = type(props)
-		render(newElement, parentConstruct)
-		return
+		return render(newElement, parentConstruct)
 	}
 
 	throw new Error(`${element.type} is not supported`)
@@ -60,21 +59,29 @@ function FunctionExample({children}) {
 	)
 }
 
+/**
+ * App from @aws-cdk/core does not exactly follow Construct constructor
+ * https://github.com/aws/aws-cdk/blob/0d7452ee3ce22179242241ed85cf55a173af19b5/packages/%40aws-cdk/core/lib/app.ts#L108
+ */
+class App extends Construct {
+}
+
 const element = (
-	// Path: top-construct
-	<Construct key="top-construct">
-		<FunctionExample>
-			{/* Path: top-construct/construct-from-function-2/inner-construct-1 */}
-			<Construct key="inner-construct-1"/>
-			{/* Path: top-construct/construct-from-function-2/inner-construct-2 */}
-			<Construct key="inner-construct-2"/>
-		</FunctionExample>
-	</Construct>
+	<App>
+		{/* Path: top-construct */}
+		<Construct key="top-construct">
+			<FunctionExample>
+				{/* Path: top-construct/construct-from-function-2/inner-construct-1 */}
+				<Construct key="inner-construct-1"/>
+				{/* Path: top-construct/construct-from-function-2/inner-construct-2 */}
+				<Construct key="inner-construct-2"/>
+			</FunctionExample>
+		</Construct>
+	</App>
 )
 
-const app = new Construct(undefined, undefined)
 
-render(element, app)
+const app = render(element)
 
 console.log(app);
 
