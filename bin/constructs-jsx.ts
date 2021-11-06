a9dda9ebef99b13d2325b3a9e1d1d15169e0ff4c
Andrew Luca <thendrluca@gmail.com>
2021-11-06 20:24:18 +0200

    init sample-app

---
+++ bin/constructs-jsx.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { ConstructsJsxStack } from '../lib/constructs-jsx-stack';
+
+const app = new cdk.App();
+new ConstructsJsxStack(app, 'ConstructsJsxStack');
